e40d2e48a175cced6522db191a3b54a25fb415f3
LokeshAditya <lokeshgandhamshr@gmail.com>
2025-07-09 13:40:32 +0530

    all committed

--- news-explorer/src/Api/Api.js
+++ news-explorer/src/Api/Api.js
@@ -1,20 +1,19 @@
 import axios from 'axios';
 
+const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+const BASE_URL = 'https://newsapi.org/v2';
+
 export const fetchTopHeadlines = async (category = '', country = 'us') => {
-  const params = new URLSearchParams({ country });
-  if (category) params.append('category', category);
+  console.log("Using API Key:", API_KEY);
+console.log("Request URL:", `${BASE_URL}/top-headlines?country=${country}&apiKey=${API_KEY}${category ? `&category=${category}` : ''}`);
 
-  const url = `/.netlify/functions/fetchNews?${params.toString()}`;
+  const url = `${BASE_URL}/top-headlines?country=${country}&apiKey=${API_KEY}${category ? `&category=${category}` : ''}`;
   const response = await axios.get(url);
-  return response.data;
+  return response.data.articles;
 };
-
 export const fetchSearchResults = async (query, category = '') => {
-  const params = new URLSearchParams({ query });
-  if (category) params.append('category', category);
-
-  const url = `/.netlify/functions/fetchNews?${params.toString()}`;
+  const url = `${BASE_URL}/everything?q=${query}${category ? `&category=${category}` : ''}&apiKey=${API_KEY}`;
   const response = await axios.get(url);
-  return response.data;
+  return response.data.articles;
 };
 
